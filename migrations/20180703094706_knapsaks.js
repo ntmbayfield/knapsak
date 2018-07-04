cfca8b019aad266b70f12be8dbb4c010407b71b1
hyperrex <mrwendell@gmail.com>
2018-07-03 23:58:04 -0700

    Update knapsaks migration file with additional modifiers

--- migrations/20180703094706_knapsaks.js
+++ migrations/20180703094706_knapsaks.js
@@ -2,15 +2,20 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('knapsaks', function(table) {
       table.increments('id').primary();
-      table.string('title');
+      table.string('title').notNullable();
+
       table.integer('user_id').unsigned();
       table.foreign('user_id')
-        .references('users.id');
-      //       .inTable('users');
+           .references('users.id')
+           .onDelete('CASCADE')
+        // .inTable('users');
+
       table.integer('kid_id').unsigned();
       table.foreign('kid_id')
-        .references('kids.id');
+           .references('kids.id')
+           .onDelete('CASCADE');
       //        .inTable('kids');
+      
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
