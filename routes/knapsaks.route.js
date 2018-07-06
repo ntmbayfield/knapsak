0bb3164dba30fb8b5344189a6b36dd2baec89201
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-07-06 15:41:23 -0700

    Change names of routes.

--- routes/knapsaks.route.js
+++ routes/knapsaks.route.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Require the controllers WHICH WE DID NOT CREATE YET!!
-// const item_controller = require('../controllers/item.controller');
 
 
-// a simple test url to check that all of our files are communicating correctly.
 router.get('/knapsaks', (req, res) => {
   database('knapsaks').select()
     .then((knapsaks) => {
