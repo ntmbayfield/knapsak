e087c62ef3e8b5d0ffddceef11fc5bd1eae60876
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-07-16 13:46:04 -0700

    Add new indexSPA.html file with separated div sections.

--- public/main.js
+++ public/main.js
@@ -1,12 +1,36 @@
 const baseURL = 'http://localhost:3333';
 
+//GET
+
 //GET A SINGLE USER BY ID
 document.querySelector('#login-submit-button').addEventListener('click', () => {
-  const userName = document.querySelector('#get-one-id-input').value
+  const userEmail = document.querySelector('#email-input').value;
+  const userID;
 
-  axios.get(`${baseURL}/users/${userid}`)
+  knex('users')
+    .where({
+      email: `${userEmail}`,
+    })
+    .select('id')
+    .then((data) => {
+      console.log('the specific user id is: ', data)
+      userID = data;
+      res.send(data);
+    }
+
+      router.get('/', (req, res, next) => {
+        console.log('users knapsaks hit')
+        knex('knapsaks')
+        .where('user_id', req.params.`${userID}`)
+        .then((data) => {
+          console.log('data', data)
+          res.send(data)
+        })
+      });
+
+  axios.get(`${baseURL}/users/:${userID}`)
     .then(result => {
       console.log(result);
     })
-    knex.('users')
+
 })
