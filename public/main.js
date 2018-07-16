49415fb5cac7bb27c1626aa96c45adf98c38e98a
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-07-15 23:56:45 -0700

    Remove duplicate html folder from repo.

--- public/main.js
+++ public/main.js
@@ -1,4 +1,12 @@
 const baseURL = 'http://localhost:3333';
 
-//GET A SINGLE USER'S DASHBOARD
-document.querySelector('#')
+//GET A SINGLE USER BY ID
+document.querySelector('#login-submit-button').addEventListener('click', () => {
+  const userName = document.querySelector('#get-one-id-input').value
+
+  axios.get(`${baseURL}/users/${userid}`)
+    .then(result => {
+      console.log(result);
+    })
+    knex.('users')
+})
